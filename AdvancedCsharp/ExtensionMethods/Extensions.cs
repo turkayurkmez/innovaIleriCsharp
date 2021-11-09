001b5f6f5a1bacaec03d332dc08145c4a8ad3714
Türkay Ürkmez <turkay@turkayurkmez.com>
2021-11-09 13:40:46 +0300

    We love extension methods....

---
+++ AdvancedCsharp/ExtensionMethods/Extensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+   public static class Extensions
+    {
+        public static int KaresiniAl(this int sayi)
+        {
+            return sayi * sayi;
+        }
+
+        public static int UssunuAl(this int sayi, int us)
+        {
+            return (int) Math.Pow(sayi, us);
+
+        }
+
+        public static string TurkceGunGetir(this DateTime date)
+        {
+            var items = new string[] { "Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi" };
+            return items[(int)date.DayOfWeek];
+        }
+
+
+    }
+}
---
+++ AdvancedCsharp/ExtensionMethods/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExtensionMethods
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int x = 8;
+            int sonuc = x.KaresiniAl();
+            int baskaSonuc = x.UssunuAl(4);
+
+            label1.Text = sonuc.ToString();
+            label2.Text = baskaSonuc.ToString();
+
+            DayOfWeek value = (DayOfWeek)6;
+            MessageBox.Show(value.ToString());
+            MessageBox.Show(DateTime.Now.TurkceGunGetir());
+        }
+    }
+}
