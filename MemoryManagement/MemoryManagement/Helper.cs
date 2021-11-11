11907476f52a5b8f41a1ddd2e0e4dd901e51a42a
Türkay Ürkmez <turkay@turkayurkmez.com>
2021-11-11 14:42:05 +0300

    introduce GC (but it's not true working)

---
+++ MemoryManagement/MemoryManagement/Helper.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MemoryManagement
+{
+    public class Helper : IDisposable
+    {
+
+        private SqlConnection sqlConnection = new SqlConnection();
+        private FileStream fileStream = null;
+
+        public void SaveProductsToFile(string path)
+        {
+            fileStream = new FileStream(path, FileMode.OpenOrCreate);
+            var array = Encoding.UTF8.GetBytes("deneme");
+            fileStream.Write(array, 0, array.Length);
+            fileStream.Close();
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.Collect();
+
+           
+
+        }
+        bool isDisposed = false;
+        void Dispose(bool isDisposing)
+        {
+            if (!isDisposed)
+            {
+                if (isDisposing)
+                {
+                    this.fileStream.Dispose();
+                    this.sqlConnection.Dispose();
+                    GC.SuppressFinalize(this);
+                }
+            }
+            isDisposed = true;
+        }
+
+        ~Helper()
+        {
+            Dispose(isDisposing: false);
+        }
+    }
+
+}
---
+++ MemoryManagement/MemoryManagement/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace MemoryManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GC.GetTotalMemory(true));
+            Console.ReadLine();
+
+            Random random = new Random();
+            int x = random.Next(1, 10);
+          
+
+            Console.WriteLine(GC.GetTotalMemory(true));
+
+            GC.Collect(0, GCCollectionMode.Forced);
+            GC.SuppressFinalize(random);
+            Console.WriteLine(GC.GetTotalMemory(true));
+
+            Helper helper = new Helper();
+            helper.SaveProductsToFile("text.txt");
+            Console.WriteLine("Dispose olmadan önce");
+            Console.WriteLine(GC.GetTotalMemory(true));
+
+            Console.WriteLine("Dispose olduktan sonra");
+
+            helper.Dispose();
+
+            Console.WriteLine(GC.GetTotalMemory(true));
+
+            Console.ReadLine();
+
+            MakeSomeGarbage();
+            Console.WriteLine("Memory used before collection:       {0:N0}",
+                              GC.GetTotalMemory(false));
+
+            // Collect all generations of memory.
+            GC.Collect();
+            Console.WriteLine("Memory used after full collection:   {0:N0}",
+                              GC.GetTotalMemory(true));
+        }
+
+        static void MakeSomeGarbage()
+        {
+            Version vt;
+
+            // Create objects and release them to fill up memory with unused objects.
+            for (int i = 0; i < 1000; i++)
+            {
+                vt = new Version();
+            }
+        }
+    }
+}
